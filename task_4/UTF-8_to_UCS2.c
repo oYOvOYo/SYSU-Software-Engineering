dededa91c355e8d7db3b231cf4dbfa9b9e4ba05d
jskyzero <jskyzero@outlook.com>
2016-09-22 13:01:49 +0800

    = =

---
+++ task_4/UTF-8_to_UCS2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void UTF_8_to_USC2(char * file_adress) {
+    FILE * fp = fopen(file_adress,"rb");
+    if(!fp) {
+        printf("File opening failed\n");
+        return;
+    }
+    while (!feof(fp)) {
+        printf("%2x",(unsigned char)fgetc(fp));  
+    }
+    fclose(fp);
+    return;
+}
+
+int main(int argc, char * argv[]) {
+    if(argc != 2) {
+        printf("Usage: ./UTF-8_to_USC2 <file> \n");
+        return -1;
+    }
+    UTF_8_to_USC2(argv[1]);
+    return 0;
+}
