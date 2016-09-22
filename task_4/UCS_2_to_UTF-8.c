e69cb1c938bd3bf9867b60898d30518555ff0c44
Nishiki <Nishiki2016@outlook.com>
2016-09-22 16:14:51 +0800

    = =

--- task_4/UCS_2_to_UTF-8.c
+++ task_4/UCS_2_to_UTF-8.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <locale.h>
 
 void UTF_8_to_USC2(char * file_adress) {
     FILE * fp = fopen(file_adress,"r");
@@ -6,18 +7,28 @@ void UTF_8_to_USC2(char * file_adress) {
         printf("File opening failed\n");
         return;
     }
+
+    char ch[2]; // store UCS_2
+    int endian = ((unsigned char)fgetc(fp) < (unsigned char)fgetc(fp));
+
     while (!feof(fp)) {
-        printf("%c",fgetc(fp));  
+        printf("%lc", ((unsigned char)fgetc(fp)<<8)+(unsigned char)fgetc(fp));
+        if(feof(fp)) break;
     }
+    printf("\n");
     fclose(fp);
     return;
 }
 
 int main(int argc, char * argv[]) {
+    setlocale(LC_ALL, "");
+
     if(argc != 2) {
         printf("Usage: ./UTF-8_to_USC2 <file> \n");
         return -1;
     }
     UTF_8_to_USC2(argv[1]);
+    
+    printf("%lc",0x24B62);
     return 0;
 }
