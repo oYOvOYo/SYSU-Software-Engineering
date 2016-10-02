e18f156fcb719c0be934268c5c62010a8ef01df3
Nishiki <jskyzero@outlook.com>
2016-10-02 14:09:59 +0800

    ver0.2

--- Text_Exercise/keyboard_exercise_class.py
+++ Text_Exercise/keyboard_exercise_class.py
@@ -1,6 +1,30 @@
-#!usr/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 class TextExercise(object):
-    def __init__(self):
+    # __init__(self, file_date, exercise_raw = 2)
+    # 初始化构造函数
+    # file_date：文件数据
+    # exercise_raw：每次练习行数
+    def __init__(self, file_date, exercise_raw = 2):
+        self.file_date_ = file_date
+        self.exercise_raw_ = exercise_raw
+        self.file_lenth_ = len(file_date)
+        self.score_ = 0
+        self.MainFuc()
+    
+    # MainFuc(self)
+    # 主函数
+    def MainFuc(self):
+        self.ShowScore()
+    
+    # Exercise(self)
+    # 练习部分
+    def Exercise(self):
         pass
+    
+    # ShowScore(self)
+    # 展示得分部分
+    def ShowScore(self):
+        print self.file_date_
+    
--- Text_Exercise/main.py
+++ Text_Exercise/main.py
@@ -1,9 +1,34 @@
-#!usr/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 from sys import argv  # for argv
 from keyboard_exercise_class import *  # for class 
 
+# PrintUsage()
+# 展示正确用法
+def PrintUsage():
+    print """Usage: ./main.py <file> [-n <num>]
+-n   :the every sigle times practice raws, default as 2"""
 
+# ReadAllFile(file_adress)
+# 读取地址文件中的所有内容
+# file_adress：文件地址
+def ReadAllFile(file_adress):
+    try:
+        f = open(file_adress, 'r')
+    except IOError: # 当文件打开出错
+        print "IOError: No such file or directory:", "%r" % file_adress
+        exit(-1)
+    return f.read()
+      
 if __name__ == "__main__":
-    print len(argv), argv 
+    if len(argv) == 4:
+        if argv[2] != "-n":
+            PrintUsage()
+        else:
+            # 注：采用关键字参数匹配模式，方便后续追加选项
+            play = TextExercise(file_date = ReadAllFile(argv[1]), exercise_raw = argv[3])
+    elif len(argv) == 2:
+        play = TextExercise(file_date = ReadAllFile(argv[1]), exercise_raw = 2)
+    else:
+        PrintUsage()
