99118578169050268dc363b3e70dc90ad86b105e
Nishiki <jskyzero@outlook.com>
2016-10-02 17:26:31 +0800

    ver 0.9

--- Text_Exercise/keyboard_exercise_class.py
+++ Text_Exercise/keyboard_exercise_class.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import math # for sqrt()
+
 class TextExercise(object):
     # __init__(self, file_date, exercise_raw = 2)
     # 初始化构造函数
@@ -8,16 +10,17 @@ class TextExercise(object):
     # exercise_raw：每次练习行数
     # line_length: 每行练习字符数
     def __init__(self, file_date, exercise_raw = '2'):
+        # 处理非ASCII字符
+        file_date = "".join(map(lambda x: ((ord(x) < 32 or ord(x) > 127) and ' ' or x ),file_date)) 
+        
         self.file_date_ = file_date
         self.exercise_raw_ = int(exercise_raw)
         self.line_length_ = 40
-
         self.file_lenth_ = len(file_date)
         self.score_ = 0
         self.total_line_number_ = 0
         self.total_entered_number_ = 0
         self.sigle_line_date_ = ""
-        
         self.MainFuc()
     
     # MainFuc(self)
@@ -58,7 +61,7 @@ class TextExercise(object):
     # UpdateScores(self)
     # 更新得分数据    
     def UpdateScores(self, LineInput):
-        for i in range(len(LineInput)):
+        for i in range(min(len(LineInput), self.line_length_)):
             self.total_entered_number_+=1
             if  self.sigle_line_date_[i] == LineInput[i]:
                 self.score_+=1
@@ -72,13 +75,12 @@ class TextExercise(object):
     # ShowScore(self)
     # 展示得分部分
     def ShowScores(self):
-        
+        correct_rate = self.score_ / float(self.line_length_ * (self.total_line_number_ - 1))
         print "$ %05d $"  % 0, ">|", '-' * self.line_length_, "|<"
-        print "Your Scores:"
         print "\tFile Length:", "%d" %self.file_lenth_
         print "\tTOTAL Correct Character:" , "%d" %self.score_
         print "\tTOTAL Entered Character:" , "%d" %self.total_entered_number_
-        print "\tTOTAL Correct Rate", "%.5f" %(
-            self.score_ / float(self.line_length_ * self.total_line_number_))
+        print "\tTOTAL Correct Rate", "%.5f" %correct_rate
+        print "Your Scores:", "%.0f" % (math.sqrt(correct_rate) * 100)
         print "$ %05d $"  % 0, ">|", '-' * self.line_length_, "|<"
     
