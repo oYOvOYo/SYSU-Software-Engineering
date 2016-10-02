041eb7e8b4d1669222a23c7ba69cd2dca41ebaee
Nishiki <jskyzero@outlook.com>
2016-10-02 09:57:35 +0800

    1 st

---
+++ Text_Exercise/main.py
@@ -0,0 +1,9 @@
+#!usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from sys import argv  # for argv
+from keyboard_exercise_class import *  # for class 
+
+
+if __name__ == "__main__":
+    print len(argv), argv 
