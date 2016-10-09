ba777fac4f2ae0422104b4f3e80ba702ce6be714
jskyzero <jskyzero@outlook.com>
2016-10-09 23:48:15 +0800

    intial

--- Agenda/model.py
+++ Agenda/model.py
@@ -1,17 +1,59 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# 储存时间
+
 import time
+import json
+
 
 class User(object):
-    def __init__(self, user_dict):
-        self.user_dict = user_dict
+    def __init__(self, user_data):
+        self.user_data = user_data
 
 class Meeting(object):
-    def __init__(self, meeting_dict):
-        self.meeting_dict = meeting_dict
+    def __init__(self, meeting_data):
+        self.meeting_data = meeting_data
 
 class Storage(object):
     def __init__(self):
-        self.read
+        self.user_list = []
+        self.meeting_list = []
+        try :
+            with open("../json/user.json",'r') as f:
+                self.user_list = json.load(f, object_hook=lambda dic: User(dic))
+        except IOError:
+            pass
+        except ValueError:
+            pass
+
+        try :
+            with open("../json/meeting.json",'r') as f:
+                self.meeting_list = json.load(f, object_hook=lambda dic: Meeting(dic))
+        except IOError:
+            pass
+        except ValueError:
+            pass
+        
+
+
+    def write_to_file(self):
+        with open("../json/user.json",'w') as f:
+            json.dump(self.user_list, f, default=lambda obj: obj.user_data, indent=2)
+        with open("../json/meeting.json",'w') as f:
+            json.dump(self.meeting_list, f, default=lambda obj: obj.meeting_data, indent=2)
+
+if __name__ == "__main__":
+    temp = Storage()
+    a = User({'name':'huang', 'list':[123,234], 'data':{1:2}})
+    b = Meeting({'name':'huang', 'list':[123,234], 'data':{1:2}})
+    print a.user_data
+    temp.user_list.append(a)
+    temp.meeting_list.append(b)
+   
+    temp.write_to_file()
+
+
+
+
+
+
---
+++ tests/test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json # for
+class Date(object):
+    def __init__(self, name, score, dic):
+        self.name, self.score, self.dic = name, score, dic
+
+    def print_score(self):
+        print "{:>10s} {:>10d}".format(self.name, self.score), self.dic
+
+def read_data(dic):
+    return dic
+    return Date(dic['name'], dic['score'], dic['dic'])
+    
+if __name__ == "__main__":
+    with open('data.json', 'r') as f:
+        temp = json.load(f, object_hook=read_data)
+        f.close()
+    print temp
+
+    obj1 = Date("huang", 100, {1:2})
+    obj2 = Date("zhang", 90, {1:2})
+    obj_list = [obj1, obj2]
+    print obj_list
+    with open('data.json', 'w') as f:
+        json.dump(obj_list, f,default=lambda obj: obj.__dict__, indent=2)
+        f.close()
+    
+         
