068705bebf689838a5720861f0cc399b04204a9a
jskyzero <jskyzero@outlook.com>
2016-10-10 21:46:01 +0800

    master to new

---
+++ task_2/task_2.md
@@ -0,0 +1,29 @@
+# Requirements for task 2 #
+
+*Copyright (c) 2016 sysu_AT. All righs reserved.*
+
+`2016-09-06`
+
+## Task 2: A Dynamic Array of Integers Implemented in C ## 
+
+### Task Goal ###
+* Learn about the usage and implementation of the vector in STL of C++
+* Train the coding skill by C language.
+* Review the memory management in C.
+* Make a habit of reading the english manuals and references.
+
+### Requirements ###
+* Fill the file [c_vector.c](./c_vector.c) and try to complie the task.
+* The behavior of all the functions of C vector should be equivalent to
+the ones of the vector&lt;int&gt; in C++.
+* Make sure that the executable file task_2 should run correctly and
+the output is the same as the comments in [main.c](./main.c).
+
+### References ###
+* http://www.cplusplus.com/reference/vector/vector/
+* http://en.cppreference.com/w/cpp/container/vector
+* https://en.wikipedia.org/wiki/Dynamic_array
+* https://google.github.io/styleguide/cppguide.html
+
+### Deadline ###
+* `2016-09-11`
---
+++ task_4/UTF-8_to_UCS-2.c
@@ -0,0 +1,72 @@
+#include <stdio.h> // for FILE fopen feof fclose 
+
+#define UCS_SIZE 2
+#define UTF_SIZE 3
+
+void UTF_8_to_UCS2(FILE * fp ) {
+   
+    FILE * out = fopen("UCS-2", "w"); // write to target
+    
+    char UCS[UCS_SIZE] = { 0xFE, 0xFF }; // store UCS-2
+    char UTF[UTF_SIZE]; // store UTF-8
+    size_t UTF_length; // store UTF-8 length
+    unsigned long Unicode; // store Unicde
+    int break_flag = 0; 
+
+    // out the file  Bid Endian or Little Endian
+    // Bid Endian : FE FF Little Endian FF FE,  this is  Bid Endian
+    fputc((unsigned char)UCS[0], out);
+    fputc((unsigned char)UCS[1], out);
+
+   
+    while (!feof(fp)) {
+    Unicode = 0;
+    // for every word, read from file and change to Unicode
+    UTF[0] = fgetc(fp);
+    if  (0 == ((unsigned char)UTF[0] >> 7)) {
+        // 0zzzzzzz（00-7F）
+        UTF_length = 1;
+        Unicode = (unsigned char)UTF[0];
+    } else if (6 == ((unsigned char)UTF[0] >> 5)) {
+        // 	110yyyyy（C0-DF) 10zzzzzz(80-BF）
+        UTF_length = 2;
+        UTF[1] = fgetc(fp);
+        Unicode = (((unsigned char)UTF[0] & 0x1f) << 6) 
+                + ((unsigned char)UTF[1] & 0x3f);
+    } else if(14 == ((unsigned char)UTF[0] >> 4)) {
+        // 	1110xxxx(E0-EF) 10yyyyyy 10zzzzzz
+        UTF_length = 3;
+        UTF[1] = fgetc(fp);
+        UTF[2] = fgetc(fp);
+        Unicode = (((unsigned char)UTF[0] & 0xf) << 12) 
+                + (((unsigned char)UTF[1] & 0x3f) << 6)
+                + ((unsigned char)UTF[2] & 0x3f);
+    } else break;
+    
+    UCS[0] = (Unicode & 0xff00) >> 8;
+    UCS[1] = (Unicode & 0xff);
+    fputc((unsigned char)UCS[0], out);
+    fputc((unsigned char)UCS[1], out);
+    }
+
+   
+    fclose(out);
+    return;
+}
+
+int main(int argc, char * argv[]) {
+    if(argc != 2) {
+        printf("Usage: ./UTF-8_to_UCS2 <file> \n");
+        return -1;
+    }
+    FILE * fp = fopen(argv[1], "r"); // read from file
+     
+     if(!fp) { // check whether the file can be open 
+        printf("File opening failed\n"); 
+        return -1;
+    } 
+
+    UTF_8_to_UCS2(fp);
+    fclose(fp);
+    return 0;
+}
