e9238b7dd2df86c8d1712f6db147c055018bbf5d
jskyzero <jskyzero@outlook.com>
2016-10-11 14:48:42 +0800

    afttertoon

--- Agenda/controller.py
+++ Agenda/controller.py
@@ -10,8 +10,8 @@ class AgendaService(object):
         self.data = Storage()
 
     def user_login(self, name, password):
-        return self.data.query_user(lambda user: name == user['name']
-        and password == user['password']) == 1
+        return len(self.data.query_user(lambda user: name == user['name']
+        and password == user['password'])) == 1
 
     def user_register(self, name, password, email, phone):
         if self.data.query_user(lambda user: name == user['name']) == 0:
@@ -33,7 +33,7 @@ class AgendaService(object):
     def list_all_users(self):
         return self.data.query_user(lambda x: True)
 
-    def create_meeting(self, name, title, start_date, end_date, participator):
+    def create_meeting(self, name, title, start_time, end_time, participator):
         try:
             str2time
             a = str2time(start_date, "%Y-%m-%d/%H:%M")
@@ -42,21 +42,26 @@ class AgendaService(object):
             return False
         if a >= b:
             return False
-        for user in participator:
-            if user.name == name:
-                return False
+        self.data.creat_meeting(Meeting(name, participator, start_time, end_time, title))
+        return True
+     
 
-    def meeting_query(self, name, start_date, end_date):
+    def meeting_query_title(self, name, title):
+        return self.data.query_meeting(lambda meeting:
+        (meeting.sponsor == name or meeting.is_participator(name)) and 
+            meeting.title == title)
+
+    def meeting_query_time(self, name, start_date, end_date):
         try:
             a = str2time(start_date, "%Y-%m-%d/%H:%M")
             b = str2time(end_date, "%Y-%m-%d/%H:%M")
         except ValueError:
             return []
-
         return self.data.query_meeting(lambda meeting:
-        (meeting.sponsor == name or meeting.is_participator(name)) and 
-        (str2time(meeting.start_time) and (str2time(meeting.end_time)))or 
-        ())
+        (meeting.sponsor == name or meeting.is_participator(name)) and (
+            (str2time(meeting.start_time) <= a and (str2time(meeting.end_time)) >= a) or 
+            (str2time(meeting.start_time) <= b and (str2time(meeting.end_time)) >= a)or
+            (str2time(meeting.start_time) <= a and (str2time(meeting.end_time)) >= b)))
 
     def list_all_meetings(self, name):
         return self.data.query_meeting(lambda meeting:
@@ -73,4 +78,4 @@ class AgendaService(object):
         return self.data.delete_meeting(lambda meeting: meeting.sponsor == name and meeting.title == title)
 
     def delete_all_meetings(self, name):
-        pass
+       return self.data.delete_meeting(lambda meeting: meeting.sponsor == name) 
--- Agenda/view.py
+++ Agenda/view.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+
+from controller import *
+
+class Agenda_UI(object):
+    def __init__(self):
+        self.controller = AgendaService()
+        self.choice = ''
+
+    def main_menu(self):
+        while not self.choice == 'q':
+            print "{:-^54}".format("Agenda")
+            print "{:<54}".format("Action")
+            
