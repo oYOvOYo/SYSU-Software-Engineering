d56da94eaf52000bf2ecd53d14ab6760648b48ba
jskyzero <jskyzero@outlook.com>
2016-10-12 21:43:31 +0800

    = =

--- Agenda/view.py
+++ Agenda/view.py
@@ -8,13 +8,10 @@ from controller import *
 class Agenda_UI(object):
     def __init__(self):
         self.controller = AgendaService()
-        self.choice = ''
+        self.choice = ""
         self.name = ""
+        self.password = ""
         self.main_func()
-
-    def get_choice(self):
-        choice = raw_input("Agenda : ~$ ")
-        self.choice = choice
 
     def main_func(self):
         while not self.choice == 'q':
@@ -23,7 +20,8 @@ class Agenda_UI(object):
             self.print_line("l", "log in Agenda by user name and passworld")
             self.print_line("r", "register an Agenda account")
             self.print_line("q", "quit Agenda")
-            self.get_choice()
+            
+            choice = raw_input("Agenda : ~$ ")
             
             if self.choice == 'l':
                 self.user_login()
@@ -60,7 +58,7 @@ class Agenda_UI(object):
             elif self.choice == "cm":
                 self.create_meeting()
             elif self.choice == "la":
-                self.list_all_meetings()
+                self.list_all_meetings()  
             elif self.choice == "las":
                 self.list_all_sponsor_meetings()
             elif self.choice == "lap":
@@ -74,53 +72,93 @@ class Agenda_UI(object):
             else:
                 print "Please check your input"
             
-            self.choice = raw_input("Agenda", self.name, " : # ")
+            self.choice = raw_input("Agenda " + self.name + " : # ")
 
-    def user_login(Self):
-        print "[login in] [user name] [password]"
-         
+    def user_login(self):
+        input_data = raw_input("[login in] [user name] [password]")
+        input_data = input_data.split("[login in] ")
+        if self.controller.user_login(input_data[0], input_data[1]):
+            self.name = input_data[0]
+            print  "success!"
+        else:
+            print "login in fail!"
+            return 
+        self.show_menu()
+        self.choice = raw_input("Agenda " + self.name + " : # ")
 
     def user_register(self):
-        pass
+        input_data = raw_input("[register] [user name] [password] [email] [phone]")
+        input_data = input_data.split("[register] ")
+
+        if self.controller.user_register(input_data[0], input_data[1], input_data[2], input_data[3]):
+            print  "success!"
+        else:
+            print "login in fail!"
 
     def quit_agenda(self):
-        pass
+        print "thanks for using Agenda"
 
     def user_logout(self):
-        pass
+        print ""
 
     def delete_user(self):
-        pass
+        print "[delete agenda account]",
+        if self.controller.delete_user(self.name, self.password):
+            print "success"
+        else:
+            print "fail to delete Agenda account!"
 
     def list_all_users(self):
-        pass
+        print "[list all user]"
+        print "{0:<15}{1:<25}{2:<15}".format("name","email", "phone")
+        for user in self.controller.list_all_users()
+            print "{0:<15}{1:<25}{2:<15}".format(user.name, user.email, user.phone)
 
     def create_meeting(self):
-        pass
+        
 
     def list_all_meetings(self):
-        pass
+        print "[list all sponsor meetings]"
+        self.print_meetings(self.controller.list_all_meetings(self.name))
 
     def list_all_sponsor_meetings(self):
-        pass
+        print "[list all sponsor meetings]"
+        self.print_meetings(self.controller.list_all_sponsor_meetings(self.name))
 
     def list_all_participate_meetings(self):
-        pass
+        print "[list all participate meetings]"
+        self.print_meetings(self.controller.list_all_participate_meetings(self.name))
 
     def querry_meeting_by_title(self):
-        pass
+        print "[queryMeeting] [title]"
+        input_data = raw_input("[queryMeeting] ")
+        self.print_meetings(self.controller.meeting_query_title(self.name, input_data))
 
     def querry_meeting_by_time_interval(self):
-        pass
+        print "[queryMeeting] [star time(yyyy-mm-dd/hh:mm)] [end time(yyyy-mm-dd/hh:mm)]"
+        input_data = raw_input("[queryMeeting] ")
+        input_data = input_data.split()
+        self.print_meetings(self.controller.meeting_query_time(input_data[0], input_data[1]))
 
     def delete_meeting_by_title(self):
-        pass
+        print "[delete meeting] [title]"
+        input_data = raw_input("[delete meeting]")
+        print "[delete meeting]"
+        if self.controller.delete_meeting(self.name, input_data):
+            print "delete meeting success!"
+        else:
+            print "delete meeting fail!"
 
     def delete_all_meetings(self):
-        pass
+        print "[delete meeting] "
+        if self.controller.delete_all_meetings(self.name):
+            print "delete meeting success!"
+        else:
+            print "delete meeting fail!"
 
-    def print_meetings(self, meeting):
-        pass
+    def print_meetings(self, meeting_list):
+        for meeting in meeting_list:
+            print
 
     def print_line(self, operation_char, operation_help):
         print "{:<5}- {:<48}".format(operation_char, operation_help)        
