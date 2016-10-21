2dd0523808806d6af7f1c77c6f9256c373d22d25
jskyzero <jskyzero@outlook.com>
2016-10-21 23:49:18 +0800

    = =

--- task_8/Part1/find_reversed_words.py
+++ task_8/Part1/find_reversed_words.py
@@ -7,8 +7,9 @@ import os # for os.path
 if __name__ == "__main__":
     with open('words', 'rt') as f:
         # 切片读词
-        while not os.path.isfile("find_reversed_words"):
-            words_list = f.read().split()
+        words_list = set(f.read().split())
+	while not os.path.isfile("find_reversed_words"):
+            words_list = set(f.read().split())
         with open('result_py', 'wt') as result:
             for word in words_list:
                 # 相等写入
