366f861400841cbd79ffee9c1aa4c115e4896644
jskyzero <jskyzero@outlook.com>
2016-11-02 19:51:53 +0800

    20161102

---
+++ Table Sorter/table-sorter/sorter.js
@@ -0,0 +1,22 @@
+document.write('<script type="text/javascript" src="../jQuery.js"></script>')
+
+(function() {
+    alert("learning");
+})();
+
+// window.onload = function() {
+//     var sortState;
+//     $("th").click(sort);
+
+// function sort() {
+//     var th = $(this);
+//     var column = th.index();
+//     var tBody = th.parents('table').find('tbody').eq(0);
+//     var tBodyChild = tBody.find('tr').toArray()
+//     th.siblings().removeClass("click");
+//     $(this).addClass("click").addClass("ascend");
+//     console.log(tBodyChild);
+//     tBody.append(tBodyChild);
+// }
+
+// }
