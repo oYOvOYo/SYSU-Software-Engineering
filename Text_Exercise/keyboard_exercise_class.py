ff8c207a7ff12d436b4af70299ff6b342aab8473
jskyzero <jskyzero@outlook.com>
2016-11-05 23:02:52 +0800

    autopep8

--- Text_Exercise/keyboard_exercise_class.py
+++ Text_Exercise/keyboard_exercise_class.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import math # for sqrt()
+import math  # for sqrt()
+
 
 class TextExercise(object):
     # __init__(self, file_date, exercise_raw = 4)
@@ -9,11 +10,12 @@ class TextExercise(object):
     # file_date：文件数据
     # exercise_raw：每次练习行数
     # line_length: 每行练习字符数
-    def __init__(self, file_date, exercise_raw = '4'):
+
+    def __init__(self, file_date, exercise_raw='4'):
         # 处理非ASCII字符
         file_date = "".join(
-            map(lambda x: ((ord(x) < 32 or ord(x) > 127) and ' ' or x), file_date)) 
-        
+            map(lambda x: ((ord(x) < 32 or ord(x) > 127) and ' ' or x), file_date))
+
         self.file_date_ = file_date
         self.exercise_raw_ = int(exercise_raw)
         self.line_length_ = 40
@@ -23,19 +25,20 @@ class TextExercise(object):
         self.total_entered_number_ = 0
         self.sigle_line_date_ = ""
         self.MainFuc()
-    
+
     # MainFuc(self)
     # 主函数
     def MainFuc(self):
         self.Exercise()
         self.ShowScores()
-    
+
     # Exercise(self)
     # 练习部分
     def Exercise(self):
         while len(self.file_date_) != 0:
-            if  self.total_line_number_ and (self.total_line_number_ % self.exercise_raw_ == 0):
-                if (self.EndExercise()): break
+            if self.total_line_number_ and (self.total_line_number_ % self.exercise_raw_ == 0):
+                if (self.EndExercise()):
+                    break
 
             if len(self.file_date_) < self.line_length_:
                 self.sigle_line_date_ = self.file_date_[:]
@@ -45,12 +48,12 @@ class TextExercise(object):
                 self.file_date_ = self.file_date_[self.line_length_:]
             self.ShowLineDate()
             self.UpdateScores(self.ReadLineInport())
-            self.total_line_number_+=1
+            self.total_line_number_ += 1
 
     # ShowLineDate(self)
-    # 展示一行数据      
+    # 展示一行数据
     def ShowLineDate(self):
-        print "$ %05d $" % (self.total_line_number_+1) , ">|", self.sigle_line_date_, "|<"
+        print "$ %05d $" % (self.total_line_number_ + 1), ">|", self.sigle_line_date_, "|<"
 
     # ReadLineInport(self)
     # 读入一行输入
@@ -59,13 +62,13 @@ class TextExercise(object):
         return raw_input()
 
     # UpdateScores(self)
-    # 更新得分数据    
+    # 更新得分数据
     def UpdateScores(self, LineInput):
         for i in range(min(len(LineInput), len(self.sigle_line_date_))):
-            self.total_entered_number_+=1
-            if  self.sigle_line_date_[i] == LineInput[i]:
-                self.score_+=1
-    
+            self.total_entered_number_ += 1
+            if self.sigle_line_date_[i] == LineInput[i]:
+                self.score_ += 1
+
     # EndExercise(self)
     # 中途停止练习
     def EndExercise(self):
@@ -75,12 +78,12 @@ class TextExercise(object):
     # ShowScore(self)
     # 展示得分部分
     def ShowScores(self):
-        correct_rate = self.score_ / float(self.line_length_ * (self.total_line_number_ ))
-        print "$ %05d $"  % 0, ">|", '-' * self.line_length_, "|<"
-        print "\tFile Length:", "%d" %self.file_lenth_
-        print "\tTOTAL Correct Character:" , "%d" %self.score_
-        print "\tTOTAL Entered Character:" , "%d" %self.total_entered_number_
-        print "\tTOTAL Correct Rate", "%.5f" %correct_rate
+        correct_rate = self.score_ / \
+            float(self.line_length_ * (self.total_line_number_))
+        print "$ %05d $" % 0, ">|", '-' * self.line_length_, "|<"
+        print "\tFile Length:", "%d" % self.file_lenth_
+        print "\tTOTAL Correct Character:", "%d" % self.score_
+        print "\tTOTAL Entered Character:", "%d" % self.total_entered_number_
+        print "\tTOTAL Correct Rate", "%.5f" % correct_rate
         print "Your Scores:", "%.0f" % (math.sqrt(correct_rate) * 100)
-        print "$ %05d $"  % 0, ">|", '-' * self.line_length_, "|<"
-    
+        print "$ %05d $" % 0, ">|", '-' * self.line_length_, "|<"
