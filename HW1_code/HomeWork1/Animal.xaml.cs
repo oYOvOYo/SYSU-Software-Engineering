106fa3790cbd22ea2c91904603012eb0b57e9db8
jskyzero <jskyzero@outlook.com>
2017-03-01 23:12:02 +0800

    add HW1

---
+++ HW1_code/HomeWork1/Animal.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
+
+namespace HomeWork1
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        // 声明事件处理的委托
+        private delegate void AnimalSpeak(string SpeakerName, bool ClearTextBox);
+        // 根据委托声明事件
+        private event AnimalSpeak SpeakEvent;
+        // 三个AnimalSpeaker的实例
+        private AnimalSpeaker cat, dog, pig;
+        
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            // 初始化实例
+            cat = new AnimalSpeaker("cat", OutputText, InputText);
+            dog = new AnimalSpeaker("dog", OutputText, InputText);
+            pig = new AnimalSpeaker("pig", OutputText, InputText);
+            // 将委托函数交给事件
+            SpeakEvent += new AnimalSpeak(cat.Speak);
+            SpeakEvent += new AnimalSpeak(dog.Speak);
+            SpeakEvent += new AnimalSpeak(pig.Speak);
+        }
+
+        static string AnimalSpeakContent(string AnimalName)
+        {
+            /// <summary>
+            /// 产生动物说话内容
+            /// </summary>
+            string SpeakContent = AnimalName.ToUpper()[0].ToString() + AnimalName.Substring(1) + ": I am " + AnimalName + ".\n";
+            return SpeakContent;
+        }
+
+        static string PickRandomAnimalName()
+        {
+            /// <summary>
+            /// 随机选取一个动物名字
+            /// </summary>
+            string[] Animals = { "cat", "dog", "pig" };
+            Random Rnd = new Random();
+            return Animals[Rnd.Next(0, Animals.Length)];
+        }
+
+        interface Animal
+        {
+            // 动物接口，只设置发言一个方法
+            void Speak(string SpeakerName, bool ClearTextBox);
+        }
+
+        class AnimalSpeaker : Animal
+        {
+            // 动物类
+            private String AnimalName;      // 动物名称
+            private TextBox ClearTarget;    // 清理目标
+            private TextBlock SpeakTarget;  // 发言目标
+
+            public AnimalSpeaker(String AnimalName, TextBlock OutputTarget, TextBox InputTarget)
+            {
+                // 构造函数，初始化私有变量
+                this.AnimalName = AnimalName;
+                this.SpeakTarget = OutputTarget;
+                this.ClearTarget = InputTarget;
+            }
+
+            public void Speak(string SpeakerName, bool ClearTextBox)
+            {
+                // 当发言人是该动物时
+                if (SpeakerName == AnimalName)
+                {
+                    // 发言
+                    SpeakTarget.Text += AnimalSpeakContent(AnimalName);
+                    // 如果要求清空输入框
+                    if (ClearTextBox)
+                    {
+                        ClearTarget.Text = "";
+                    }
+                }
+            }
+        }
+
+        private void SpeakButton_Click(object sender, RoutedEventArgs e)
+        {
+            // 点击Speak按钮处理函数
+            SpeakEvent(PickRandomAnimalName(), false);
+        }
+
+        private void OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            // 点击OK按钮处理函数
+            SpeakEvent(InputText.Text, true);
+        }
+
+        private void HyperlinkButton_Click(object sender, RoutedEventArgs e)
+        {
+            // 跳转到Main.xaml
+            this.Frame.Navigate(typeof(Main));
+        }
+    }
+}
---
+++ HW1_code/HomeWork1/Main.xaml.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// WTF
+using Windows.UI.Xaml.Media.Imaging;
+
+// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
+
+namespace HomeWork1
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class Main : Page
+    {
+        // Player Request 
+        enum UserRequest { NO_REQUEST, LET_ME_WIN, LET_ME_LOSE};
+        // Store Your Request
+        private UserRequest YouWant;
+        // delegate and event
+        private delegate void Game(object sender, AnimalName e);
+        private Game GameStart;
+
+        public Main()
+        {
+            this.InitializeComponent();
+            // intial values
+            YouWant = UserRequest.NO_REQUEST;
+            GameStart += AnimalGame;
+        }
+
+        private void AnimalGame(object sender, AnimalName e)
+        {
+            InitialGame(sender, e);
+            // get ans and update content
+            string YourAns = e.Args;
+            string RightAns = PickRandomAnimalName();
+
+            if (YouWant == UserRequest.LET_ME_WIN)
+            {
+                while (RightAns != YourAns)
+                {
+                    RightAns = PickRandomAnimalName();
+                }
+            }
+
+            if (YouWant == UserRequest.LET_ME_LOSE)
+            {
+                while (RightAns == YourAns)
+                {
+                    RightAns = PickRandomAnimalName();
+                }
+            }
+
+            YouWant = UserRequest.NO_REQUEST;
+
+            UpdateImg(sender, YourAns, RightAns);
+            // show ans 
+            if (YourAns == RightAns)
+            {
+                DispalyWinMessage(sender);
+            } else
+            {
+                DisplatLoseMessage(sender);
+            }
+            
+        }
+
+        private void InitialGame(object sender, RoutedEventArgs e)
+        {
+            // Img
+            YourImg.Opacity = 0;
+            RightImg.Opacity = 0;
+            // Ans
+            WinText.Opacity = 0;
+            WinImg.Opacity = 0;
+            LoseText.Opacity = 0;
+            LoseImg.Opacity = 0;
+        }
+
+        private void UpdateImg(object sender, string YourAns, string RightAns)
+        {
+            // rource and opacity
+            string WhereIsImg = "ms-appx:/Assets/Img.png";
+            YourImg.Source = new BitmapImage(new Uri(WhereIsImg.Replace("Img", YourAns), UriKind.RelativeOrAbsolute));
+            RightImg.Source = new BitmapImage(new Uri(WhereIsImg.Replace("Img", RightAns), UriKind.RelativeOrAbsolute));
+            YourImg.Opacity = 1;
+            RightImg.Opacity = 1;
+        }
+
+        private void DispalyWinMessage(object sender)
+        {
+            WinText.Opacity = 1;
+            WinImg.Opacity = 1;
+        }
+
+        private void DisplatLoseMessage(object sender)
+        {
+            LoseText.Opacity = 1;
+            LoseImg.Opacity = 1;
+        }
+
+
+        private void CatButton_Click(object sender, RoutedEventArgs e)
+        {
+            GameStart(sender, new AnimalName("cat"));
+        }
+
+        private void DogButton_Click(object sender, RoutedEventArgs e)
+        {
+            GameStart(sender, new AnimalName("dog"));
+
+        }
+
+        private void PigButton_Click(object sender, RoutedEventArgs e)
+        {
+            GameStart(sender, new AnimalName("pig"));
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            // back to Mainpage
+            this.Frame.Navigate(typeof(MainPage));
+        }
+
+        private void LetMeWin_Click(object sender, RoutedEventArgs e)
+        {
+            YouWant = UserRequest.LET_ME_WIN;
+        }
+
+        private void LetMeLose_Click(object sender, RoutedEventArgs e)
+        {
+            YouWant = UserRequest.LET_ME_LOSE;
+        }
+
+        static string PickRandomAnimalName()
+        {
+            /// <summary>
+            /// 随机选取一个动物名字
+            /// </summary>
+            string[] Animals = { "cat", "dog", "pig" };
+            Random Rnd = new Random();
+            return Animals[Rnd.Next(0, Animals.Length)];
+        }
+
+        class AnimalName : RoutedEventArgs
+        {
+            private String Name;
+
+            public AnimalName(String _Name)
+            {
+                Name = _Name;
+            }
+
+            public string Args
+            {
+                get
+                {
+                    return Name;
+                }
+            }
+
+        }
+
+    }
+}
