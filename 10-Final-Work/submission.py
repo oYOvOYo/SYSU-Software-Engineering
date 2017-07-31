7d48af4fe4315d1d715d07709ca5e1f987d40a3e
jskyzero <jskyzero@outlook.com>
2017-07-31 09:49:33 +0800

    add final work

---
+++ 10-Final-Work/submission.py
@@ -0,0 +1,12 @@
+## import modules here 
+
+
+################# training #################
+
+def train(data, classifier_file):# do not change the heading of the function
+    pass # **replace** this line with your code    
+
+################# testing #################
+
+def test(data, classifier_file):# do not change the heading of the function
+    pass # **replace** this line with your code
