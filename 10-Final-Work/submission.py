13f83e330a10090897f3c898a07b8d59bab837f3
jskyzero <jskyzero@outlook.com>
2017-08-08 10:04:42 +0800

    change MultinomialNB feature

--- 10-Final-Work/submission.py
+++ 10-Final-Work/submission.py
@@ -6,6 +6,59 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.externals import joblib
 
+VOWELS_TABLE = """\
+|   AA  |   ɑ   |
+|   AE  |   æ   |
+|   AH  |   ʌ   |
+|   AO  |   ɔ   |
+|   AW  |   aʊ  |
+|   AY  |   aɪ  |
+|   EH  |   ɛ   |
+|   ER  |   ɜːr |
+|   EY  |   eɪ  |
+|   IH  |   ɪ   |
+|   IY  |   i   |
+|   OW  |   oʊ  |
+|   OY  |   ɔɪ  |
+|   UH  |   ʊ   |
+|   UW  |   u   |
+"""
+
+CONSONANT_TABLE = """\
+|     P     |  p  |
+|     S     |  s  |
+|     B     |  b  |
+|     SH    |  ʃ  |
+|     CH    |  tʃ |
+|     T     |  t  |
+|     D     |  d  |
+|     TH    |  θ  |
+|     DH    |  ð  |
+|     V     |  v  |
+|     F     |  f  |
+|     W     |  w  |
+|     G     |  g  |
+|     Y     |  j  |
+|     HH    |  h  |
+|     Z     |  z  |
+|     JH    |  dʒ |
+|     ZH    |  ʒ  |
+|     K     |  k  |
+|     L     |  l  |
+|     M     |  m  |
+|     N     |  n  |
+|     NG    |  ŋ  |
+|     R     |  r  |
+"""
+def init_phonemes(phonemes_table):
+    ans = {}
+    phonemes_list = list(filter(lambda x: not x in "\n",
+                              phonemes_table.replace(" ", "").split("|")))
+    for x in range(0, len(phonemes_list), 2):
+        ans[phonemes_list[x]] =  phonemes_list[x+1]
+    return ans
+
+
 
 def get_world(line):
     return line.split(":")[0]
@@ -25,16 +78,16 @@ def get_data_1(line):
     vowels_list = list(filter(lambda phoneme: has_number(phoneme), phonemes_list))
     # return [get_stress_index(vowels_list), line.replace("0", "").replace("1", "").replace("2", "")]
     for i in range(len(phonemes_list)):
-        phonemes_list[i] = str(i) + phonemes_list[i]
+        phonemes_list[i] = str(i) + phonemes_list[i].replace("0", "").replace("1", "").replace("2", "") + (str(vowels_list.index(phonemes_list[i])) if phonemes_list[i] in vowels_list else "") 
     return [get_stress_index(vowels_list), " ".join(phonemes_list)]
 
-def get_data_2(line):
+def get_data_2(line, vowels_dict):
     line = get_phonemes(line)
     phonemes_list = line.split(" ")
-    vowels_list = list(filter(lambda phoneme: has_number(phoneme), phonemes_list))
+    vowels_list = list(filter(lambda phoneme: phoneme in vowels_dict, phonemes_list))
     # return [get_stress_index(vowels_list), line.replace("0", "").replace("1", "").replace("2", "")]
     for i in range(len(phonemes_list)):
-        phonemes_list[i] = str(i) + phonemes_list[i]
+        phonemes_list[i] = str(i) + phonemes_list[i].replace("0", "").replace("1", "").replace("2", "") + (str(vowels_list.index(phonemes_list[i])) if phonemes_list[i] in vowels_list else "") 
     return [0, " ".join(phonemes_list)]
 
 def get_freq_of_tokens(sms):
@@ -69,12 +122,14 @@ def train(data, classifier_file):# do not change the heading of the function
 ################# testing #################
 
 def test(data, classifier_file):# do not change the heading of the function
+    vowels_dict = init_phonemes(VOWELS_TABLE)
     nb, encoder, vectorizer = joblib.load(classifier_file)
     
     train_data = data
-    train = pd.DataFrame([get_data_2(line) for line in train_data], 
+    train = pd.DataFrame([get_data_2(line, vowels_dict) for line in train_data], 
                   index=[get_world(line) for line in train_data],
                   columns=["type", "text"])
+    print(train.head())
     x_train = [ get_freq_of_tokens(text) for text in  train.text ]
     x = vectorizer.transform(x_train)
 
