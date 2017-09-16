2bf0e9ee29476288d2ee2735713bb88b1e656c6b
jskyzero <jskyzero@outlook.com>
2017-09-16 09:58:41 +0800

    UI finished

--- js/main.js
+++ js/main.js
@@ -1,25 +1,31 @@
-$(document).ready( function() {
+$(document).ready(function () {
   console.log("Welcome to Color-of-Mood");
 
   // $(".alert").alert('close');
 
 
-  // bootstrap_alert = function() {}
-  // bootstrap_alert.warning = function(message) {
-  //             $('#alert_placeholder').html('<div class="alert"><a class="close" data-dismiss="alert">×</a><span>'+message+'</span></div>')
-  //         }
-  // bootstrap_alert.warning('Your text goes here');
-  // });
-
-  function httpGet(theUrl) {
-      var xmlHttp = new XMLHttpRequest();
-      xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-      xmlHttp.send( null );
-      return xmlHttp.responseText;
+  bootstrap_alert = function () { }
+  bootstrap_alert.warning = function (message) {
+    $('#alert_box').append('\
+      <div class="alert alert-warning alert-dismissible fade show" role="alert">\
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">\
+          <span aria-hidden="true">&times;</span>\
+        </button>\
+        <strong>请注意</strong> 开始游戏前需要在设置绑定对方的IP地址\
+       </div>');
   }
+  bootstrap_alert.warning('Your text goes here');
+  bootstrap_alert.warning('Your text goes here');
 
-  url="https://api.ipify.org/?format=json";
-  console.log(JSON.parse(httpGet(url)).ip);
+function httpGet(theUrl) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", theUrl, false); // false for synchronous request
+  xmlHttp.send(null);
+  return xmlHttp.responseText;
+}
+
+url = "https://api.ipify.org/?format=json";
+console.log(JSON.parse(httpGet(url)).ip);
 })
 
 
