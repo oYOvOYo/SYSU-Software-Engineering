64ac461e14a2aed887092eb8bb67bdfcdc536034
jskyzero <jskyzero@outlook.com>
2017-09-16 11:02:14 +0800

    logic part begin

--- js/main.js
+++ js/main.js
@@ -1,31 +1,47 @@
 $(document).ready(function () {
+  'use strict';
+
+  var ip_url = "http://ip-api.com/json";
+
   console.log("Welcome to Color-of-Mood");
 
+  httpGetAsync(ip_url, responseText => {
+    $('#settingsModal .input-group input.me').val(JSON.parse(responseText).query);
+    console.log(JSON.parse(responseText).query);
+  })
+
+  $('#menu .begin')[0].disabled = false;
+
   // $(".alert").alert('close');
-
-
-  bootstrap_alert = function () { }
-  bootstrap_alert.warning = function (message) {
+  var bootstrap_alert = function () { }
+  bootstrap_alert.alert = (type, strong, message) => {
     $('#alert_box').append('\
-      <div class="alert alert-warning alert-dismissible fade show" role="alert">\
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">\
-          <span aria-hidden="true">&times;</span>\
-        </button>\
-        <strong>请注意</strong> 开始游戏前需要在设置绑定对方的IP地址\
-       </div>');
+    <div class="alert alert-'+ type + ' alert-dismissible fade show" role="alert">\
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">\
+        <span aria-hidden="true">&times;</span></button>\
+      <strong>'+ strong + '</strong>' + message + '</div>');
   }
-  bootstrap_alert.warning('Your text goes here');
-  bootstrap_alert.warning('Your text goes here');
+  bootstrap_alert.warning = (strong, message) => {
+    bootstrap_alert.alert('warning', strong, message);
+  }
+  bootstrap_alert.warning('Your text goes here','orz');
 
-function httpGet(theUrl) {
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.open("GET", theUrl, false); // false for synchronous request
-  xmlHttp.send(null);
-  return xmlHttp.responseText;
-}
+  function httpGet(theUrl) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", theUrl, false); // false for synchronous request
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+  }
 
-url = "https://api.ipify.org/?format=json";
-console.log(JSON.parse(httpGet(url)).ip);
+  function httpGetAsync(theUrl, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+      if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+        callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+  }
 })
 
 
