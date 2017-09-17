09db9d0bfeb24ae6dcab3a50d9cc6e0f993b74f0
jskyzero <jskyzero@outlook.com>
2017-09-17 22:42:59 +0800

    change ip api to hrrps

--- js/main.js
+++ js/main.js
@@ -5,11 +5,10 @@ $(document).ready(function () {
   var ip_url = "http://ip-api.com/json";
 
   console.log("Welcome to Color-of-Mood");
-
-  httpGetAsync(ip_url, responseText => {
-    $('#settingsModal .input-group input.me').val(JSON.parse(responseText).query);
-    console.log(JSON.parse(responseText).query);
-  })
+  $.get(ip_url,responseText => {
+    $('#settingsModal .input-group input.me').val(responseText.query);
+    console.log("Your ip is ", responseText.query);
+  });
 
   $('#settingsModal button.btn.btn-primary.bindding').on( "click", ()=>{
     alert.warning('请注意','开始游戏前请在设置里面绑定对方IP');
@@ -29,24 +28,6 @@ $(document).ready(function () {
     this.warning = (strong, message) => {
       this.alert('warning', strong, message);
     }
-  }
-  
-
-  function httpGet(theUrl) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", theUrl, false); // false for synchronous request
-    xmlHttp.send(null);
-    return xmlHttp.responseText;
-  }
-
-  function httpGetAsync(theUrl, callback) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function () {
-      if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-        callback(xmlHttp.responseText);
-    }
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
-    xmlHttp.send(null);
   }
 })
 
