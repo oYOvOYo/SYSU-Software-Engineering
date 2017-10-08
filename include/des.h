ee34f1b5933c81a6f243629c6dd29269cbce6129
jskyzero <jskyzero@outlook.com>
2017-10-08 16:30:33 +0800

    add feistel function

--- include/des.h
+++ include/des.h
@@ -12,4 +12,9 @@
 #include <string.h>
 
 // process file
-void process_unit(PROCESS_TYPE process_type, uint64_t* sub_keys, uint8_t* block_unit);
+void process_unit(PROCESS_TYPE process_type, uint64_t* sub_keys,
+                  uint8_t* block_unit);
+// iteration
+void iteration_unit(uint64_t* block, uint64_t sub_key);
+// feistel function
+uint64_t feistel(uint64_t* num);
