d55281458698a66d4dc5c92f1d835647922f1560
jskyzero <jskyzero@outlook.com>
2017-10-16 15:11:35 +0800

    add main.c

---
+++ md5.c
@@ -0,0 +1,12 @@
+/*
+ * MD5 algorithm implemented in C.
+ * jskyzero 2017/10/16 SYSU
+ * 
+ */
+
+#include <stdio.h>
+
+ int main(int argc, char*argv[]) {
+  printf("%s\n", argv[1]);
+  return 0;
+ }
