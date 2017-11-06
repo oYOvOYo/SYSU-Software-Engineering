f620c8931f358298bef12ea7f40f38ff94a1edee
jskyzero <jskyzero@outlook.com>
2017-11-06 19:06:29 +0800

    add 单向数据绑定/双向数据绑定。

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Hello';
+  title = 'Work-Better';
+  hero: Hero = {
+    id: 1,
+    name: 'hero1'
+  };
+}
+
+export class Hero {
+  id: number;
+  name: string;
 }
