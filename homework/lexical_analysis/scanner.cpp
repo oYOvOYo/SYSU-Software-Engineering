5a7b2021e10cc5158ad56b69fdc33598b848cbf5
jskyzero <jskyzero@outlook.com>
2018-01-10 14:43:12 +0800

    逐个处理之后的字符，直到出现不属于该类型标记字符集中的字符（即最长一致原则）。

--- homework/lexical_analysis/scanner.cpp
+++ homework/lexical_analysis/scanner.cpp
@@ -45,7 +45,7 @@ bool isKeyChar(char ch) {
   return false;
 }
 
-bool is_identifieres(const string & str) {
+bool is_identifieres(const string& str) {
   if (str.empty()) return false;
   if (!isalpha(str[0])) return false;
   for (auto ch : str) {
@@ -54,15 +54,56 @@ bool is_identifieres(const string & str) {
   return true;
 }
 
-
-bool is_Unsigned_integer(const string & str) {
+bool is_Unsigned_integer(const string& str) {
   if (str.empty()) return false;
   for (auto ch : str) {
     if (!isdigit(ch)) return false;
   }
   return true;
 }
+// check can be process
+bool can_process(const string& str) {
+  for (int i = 0; i < keywords.number; i++) {
+    if (keywords.str[i] == str) {
+      return true;
+    }
+  }
 
+  for (int i = 0; i < operators.number; i++) {
+    if (operators.str[i] == str) {
+      return true;
+    }
+  }
+
+  for (int i = 0; i < boundaries.number; i++) {
+    if (boundaries.str[i] == str) {
+      return true;
+    }
+  }
+
+  for (int i = 0; i < identifieres.number; i++) {
+    if (identifieres.str[i] == str) {
+      return true;
+    }
+  }
+
+  for (int i = 0; i < Unsigned_integer.number; i++) {
+    if (Unsigned_integer.str[i] == str) {
+      return true;
+    }
+  }
+
+  if (is_identifieres(str)) {
+    return true;
+  }
+
+  if (is_Unsigned_integer(str)) {
+    return true;
+  }
+
+  return false;
+}
+// find properol str type
 void process_str(string& str) {
   // cout << str << endl;
   for (int i = 0; i < keywords.number; i++) {
@@ -100,14 +141,14 @@ void process_str(string& str) {
     }
   }
 
-  if(is_identifieres(str)) {
+  if (is_identifieres(str)) {
     cout << "<4," << identifieres.number << ">";
     identifieres.str[identifieres.number] = str;
     identifieres.number++;
     str = "";
   }
 
-  if(is_Unsigned_integer(str)) {
+  if (is_Unsigned_integer(str)) {
     cout << "<5," << Unsigned_integer.number << ">";
     Unsigned_integer.str[Unsigned_integer.number] = str;
     Unsigned_integer.number++;
@@ -120,26 +161,29 @@ int main() {
   string before = "";
 
   while (scanf("%c", &ch), ch != '#') {
-    if (isKeyChar(ch)) {
-      process_str(before);
-      if (!isspace(ch)) {
-        before += ch ;
+      if (isspace(ch)) {
         process_str(before);
-      }
-    } else {
-      if (!isspace(ch)) {
-        before += ch ;
+      } else {
+      if (can_process(before + ch)) {
+        before += ch;
+      } else {
+        // use longest principle
+        process_str(before);
+        before += ch;
       }
     }
   }
+  // process last when # enter
+  process_str(before);
+
   cout << endl;
   cout << "identifieres:";
   for (int i = 0; i < identifieres.number; i++) {
-    cout << (i == 0 ? "" :" ") << identifieres.str[i];
+    cout << (i == 0 ? "" : " ") << identifieres.str[i];
   }
   cout << endl;
   cout << "Unsigned_integer:";
   for (int i = 0; i < Unsigned_integer.number; i++) {
-    cout << (i == 0 ? "" :" ") << Unsigned_integer.str[i];
+    cout << (i == 0 ? "" : " ") << Unsigned_integer.str[i];
   }
 }
